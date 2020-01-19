db1143594b12ee86c78a505220f212d4befcac8b
Glelg <gkarpukhin@gmail.com>
2020-01-20 00:42:01 +0300

    Done tasks variant and type_map

--- type_map/include/type_map.hpp
+++ type_map/include/type_map.hpp
@@ -30,16 +30,22 @@ namespace detail
 
 //? What is wrong with ``*this[i]``?
 //{
-template<???>
-class type_map: public ???
+template<class A, class ...Args>
+class type_map : public std::array<A, sizeof...(Args)>
 {
 public:
-    using types = ...
+    using types = std::tuple<Args...>;
 
     template<class T>
-    ?? as()
+    A& as()
     {
-        return ...
+        return const_cast<A&>(const_cast<const type_map*>(this)->as<T>());
+    }
+
+    template<class T>
+    const A& as() const
+    {
+        return (*this)[detail::type_to_index<T, types>::v];
     }
 };
 //}
@@ -47,11 +53,9 @@ public:
 namespace std
 {
     template<typename T, class TypeMap>
-    ?? get(??TypeMap?? tm)
+    auto& get(TypeMap& tm)
     {
-        //{ How to call ``as``?
-        return ???
-        //}
+        return tm.template as<T>();
     }
 }
 
